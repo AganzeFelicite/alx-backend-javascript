ccd4d022a1fd69410a607a581767e625a08df52d
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-03 17:25:02 +0200

    rest-parameters

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let total = 0;
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < args.length; i++) {
-    total += 1;
-  }
-  console.log(total);
+  return args.length;
 }
