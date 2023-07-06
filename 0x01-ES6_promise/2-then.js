f19082ecba8340e6df1ec381ec828f5fb2d5156d
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-06 12:29:48 +0200

    fixed Success != success error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,7 @@
 
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'Success' }))
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
