a1cf1cefd004799121c5d230ee6089293a009fd6
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-06 11:57:26 +0200

    using call backs

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,9 +3,9 @@
  * and perform some activities on the function
  */
 
-const error = () => new Error();
-const successfull = () => ({ status: 200, body: 'Success' });
-const final = () => { console.log('Got a response from the API'); };
 export default function handleResponseFromAPI(promise) {
+  const error = () => new Error();
+  const successfull = () => ({ status: 200, body: 'Success' });
+  const final = () => { console.log('Got a response from the API'); };
   promise.then(successfull).catch(error).finally(final);
 }
