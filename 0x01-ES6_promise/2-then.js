b4077a2e8e81a837fa9ae16023ce7cb26174a87f
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-06 12:11:14 +0200

    returning call backs

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,7 +4,7 @@
  */
 
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => new Error()).finally(
-    console.log('Got a response from the API'),
-  );
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => new Error()).finally(() => {
+    console.log('Got a response from the API');
+  });
 }
