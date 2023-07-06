9b6ea62987c2a2e0844b46a34cddbaf539866438
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-06 09:35:42 +0200

    Don't make a promise...if you know you can't keep it mandatory

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,22 @@
+/**
+ * this is a function that retuns a promise that has
+ * arg1 : success a boolean and
+ * returns an object on success and
+ * a string on failuere
+ */
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve(
+        {
+          status: 200,
+          body: 'Success',
+
+        },
+      );
+    } else {
+      reject('The fake API is not working currently');
+    }
+  });
+}
