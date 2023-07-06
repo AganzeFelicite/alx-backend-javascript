3c594d0ee7b7552d5d112b31191c7d5bd9cc3002
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-06 11:21:27 +0200

    then and catch

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+/**
+ * this is a function that receives a promise and
+ * and perform some activities on the function
+ */
+
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return (
+      {
+        status: 200,
+        body: 'Success',
+
+      }
+    );
+  }).catch(() => new Error());
+}
