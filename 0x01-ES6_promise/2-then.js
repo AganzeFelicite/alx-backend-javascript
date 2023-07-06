8cb4268c8be79231913aecb1d14451c3f1f29317
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-06 11:36:23 +0200

    some changes on the return

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,11 +4,7 @@
  */
 
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'Success',
-
-  })).catch(new Error()).finally(() => {
-    console.log('Got a response from the API');
-  });
+  promise.then(() => ({ status: 200, body: 'Success' })).catch(new Error()).finally(
+    console.log('Got a response from the API'),
+  );
 }
