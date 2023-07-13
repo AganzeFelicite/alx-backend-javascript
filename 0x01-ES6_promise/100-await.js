c83a4d11a33fa3493470f8ff009d981102ec7e9d
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-13 17:39:02 +0200

    using await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -5,15 +5,26 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default async function asyncUploadUser() {
-  const promise = [createUser(), uploadPhoto()];
-  return Promise.all(promise)
-    .then((resultArr) => ({
-      status: resultArr[1],
-      user: resultArr[0],
-    }))
-    .catch(() => ({
+//   const promise = [createUser(), uploadPhoto()];
+//   return Promise.all(promise)
+//     .then((resultArr) => ({
+//       status: resultArr[1],
+//       user: resultArr[0],
+//     }))
+//     .catch(() => ({
+//       status: null,
+//       user: null,
+
+  //     }));
+  try {
+    return {
+      status: await createUser(),
+      user: await uploadPhoto(),
+    };
+  } catch (e) {
+    return {
       status: null,
       user: null,
-
-    }));
+    };
+  }
 }
