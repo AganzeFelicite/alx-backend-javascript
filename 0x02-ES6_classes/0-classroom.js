c622391095f5f0dfaf8dfa4d12bb825c5b67576e
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-18 10:00:00 +0200

    inintial commit

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/**
+ * this is a class that define a classroom
+ * object with maximum size
+ */
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
