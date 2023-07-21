0f51c0c03392b2f4950d21e62db8752fee835c22
AganzeFelicite <aganzefelicite@gmail.com>
2023-07-21 14:26:10 +0200

    some change

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,13 +4,15 @@
 
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof (name) !== 'string') {
-      throw new TypeError('Name must be a string');
-    } else if (typeof (length) !== 'number') {
-      throw new TypeError('Length must be a number');
-    } else if ((!Array.isArray(students) || students.every((student) => typeof student !== 'string'))) {
-      throw new TypeError('Students must be an array of strings');
-    }
+    // if (typeof (name) !== 'string') {
+    //   throw new TypeError('Name must be a string');
+    // } else if (typeof (length) !== 'number') {
+    //   throw new TypeError('Length must be a number');
+    // } else if ((!Array.isArray(students) || students.every((student)
+
+    //= > typeof student !== 'string'))) {
+    //   throw new TypeError('Students must be an array of strings');
+    // }
     /* eslint-disable */
     this._name = name;
     this._length = length;
