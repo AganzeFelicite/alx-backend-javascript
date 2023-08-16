73a693ed2f4f0efbaf2d32a6aa76a69763102442
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-16 17:49:42 +0200

    this is some a typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -9,8 +9,8 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
 
-  return buffer;
+  return view;
 }
