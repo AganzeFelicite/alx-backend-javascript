1fcac3fdb644868b6c088525fd8963d236a498df
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-16 17:44:17 +0200

    this is some a typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,10 @@
  */
 
 export default function createInt8TypedArray(length, position, value) {
-  if (typeof length !== 'number' || typeof position !== 'number' || typeof value !== 'number') throw new TypeError();
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
   view[position] = value;
