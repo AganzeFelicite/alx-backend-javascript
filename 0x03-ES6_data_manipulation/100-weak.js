14caf0d6f0e93471c40192deb7146dcc43c760a3
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-17 12:04:05 +0200

    weak maps for api calls

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endPoint) {
   if (weakMap.has(endPoint)) {
     const call = weakMap.get(endPoint);
-    if (call >= 5) {
+    if (call >= 4) {
       throw new Error('Endpoint load is high');
     }
     weakMap.set(endPoint, call + 1);
