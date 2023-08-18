28e26ed70abfead0faf0a7463a57a0c6cc427174
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-18 15:52:44 +0200

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,6 +51,24 @@ function createEmployee (salary : number | string) : Director | Teacher{
 
     }
 
-    console.log(createEmployee(200));
-    console.log(createEmployee(1000));
-    console.log(createEmployee('$500'));
+    // console.log(createEmployee(200));
+    // console.log(createEmployee(1000));
+    // console.log(createEmployee('$500'));
+
+    function isDirector(employee : Teacher | Director) : boolean{
+     return employee instanceof Director;
+    }
+
+    function executeWork(employee : Teacher | Director) : string{
+        if (employee instanceof Director){
+            return employee.workDirectorTasks();
+        }
+        if (employee instanceof Teacher){
+            return employee.workTeacherTasks();
+        }
+    }
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)))
+
+
