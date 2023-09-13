6618f0a009d596d79d4830d9195d966b4d2c734b
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-13 02:45:07 +0200

    trying to fix the error

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,9 @@
-
 /**
  * function to display whatever argument to the STDOUT
  * @param a
  */
 function displayMessage(a){
-    console.log(a)
+    console.log(a);
 }
 
 module.exports = displayMessage;
