8eb06a7cf7d4717fbbdf3b179fb9a2f6cb2e1ef6
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-15 07:56:11 +0200

    some sever done

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,72 +1,95 @@
 const http = require('http');
-const { readFile } = require('fs');
+const fs = require('fs');
 
-const PORT = 1245;
-const HOSTNAME = 'localhost';
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = http.createServer();
+const path = process.argv.length > 2 ? process.argv[2] : '';
 
-function countStudents(fileName) {
-  const course = {};
-  const fields = {};
-  let length = 0;
+function countStudents(path) {
   return new Promise((resolve, reject) => {
-    const response = [];
-    readFile(fileName, (error, data) => {
-      if (error) {
-        reject(Error('Cannot load the database'));
-      } else {
-        const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(course, field[3])) {
-              course[field[3]].push(field[0]);
-            } else {
-              course[field[3]] = [field[0]];
-            }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
-            } else {
-              fields[field[3]] = 1;
-            }
-          }
-        }
-        const l = length - 1;
-        response.push(`Number of students: ${l}`);
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            response.push(`Number of students in ${key}: ${value}. List: ${course[key].join(', ')}`);
-          }
-        }
-        resolve(response.join('\n'));
+    if (!path) {
+      reject(new Error('Cannot load the database'));
+    }
+
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
+
+      const lines = data.toString().trim().split('\n').slice(1);
+
+      let count = 0;
+
+      const fields = {};
+
+      lines.forEach((line) => {
+        count += 1;
+
+        const studentRecord = line.split(',');
+
+        if (!(studentRecord[3] in fields)) fields[studentRecord[3]] = [];
+
+        fields[studentRecord[3]].push(studentRecord[0]);
+      });
+      let result = `Number of students: ${count}\n`;
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        }
+      }
+      result = result.trim();
+
+      resolve(result);
     });
   });
 }
 
-module.exports = countStudents;
+function handleRootRoute(req, res) {
+  const responseMessage = 'Hello Holberton School!';
 
-const app = http.createServer((req, res) => {
-  const { url } = req;
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseMessage.length);
+  res.statusCode = 200;
 
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  } else if (url === '/students') {
-    res.write('This is the list of our students\n');
-    countStudents(process.argv[2].toString())
-      .then((data) => {
-        res.end(data);
-      })
-      .catch(() => {
-        res.statusCode = 404;
-        res.send('Cannot load the database');
-      });
+  res.write(Buffer.from(responseMessage));
+}
+
+async function handleStudentRoute(req, res) {
+  let responseMessage = ['This is the list of our students'];
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+
+  await countStudents(path)
+    .then((result) => {
+      responseMessage.push(result);
+
+      responseMessage = responseMessage.join('\n');
+    })
+    .catch(() => {
+      responseMessage = 'This is the list of our students\nCannot load the database';
+    });
+
+  res.setHeader('Content-Length', responseMessage.length);
+
+  res.write(Buffer.from(responseMessage));
+
+  res.end();
+}
+
+app.on('request', (req, res) => {
+  if (req.url === '/') {
+    handleRootRoute(req, res);
+  } else {
+    handleStudentRoute(req, res);
   }
 });
 
-app.listen(PORT, HOSTNAME, () => {});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
